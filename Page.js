8acef69be3e647b2a0a450503683ff04e95f588e
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-04-07 14:56:20 +0100

    First commit

---
+++ Page.js
@@ -0,0 +1,9 @@
+let dashBoard = document.querySelector('#dashBoard');
+let courses = document.querySelector('#courses');
+let takeExam = document.querySelector('#takeExam');
+let downloadResult = document.querySelector('#downloadResult');
+let content = document.querySelector('#content');
+
+dashBoard.addEventListener('click', () => {
+    content.innerTEXT = 'TTTTTTTB';
+});
